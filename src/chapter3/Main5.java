ec10965f4eb62b88d5aff32ba2c4750b110ec652
anian1989 <junshuaizhang@163.com>
2014-09-30 15:11:22 +0800

    Phraser

---
+++ src/chapter3/Main5.java
@@ -0,0 +1,43 @@
+package chapter3;
+
+import java.util.concurrent.Phaser;
+
+public class Main5 {
+
+	/**
+	 * <p>
+	 * </p>
+	 * @author zhangjunshuai
+	 * @date 2014-9-29 ÏÂÎç4:31:46
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Phaser phaser = new Phaser();
+		
+		FileSearch system = new FileSearch("D:\\logs","log",phaser);
+		FileSearch apps = new FileSearch("c:\\Program Files","log",phaser);
+		FileSearch documents = new FileSearch("c:\\Documents And Settings","log",phaser);
+		
+		Thread systemThread = new Thread(system,"System");
+		systemThread.start();
+		
+		Thread appsThread = new Thread(apps,"apps");
+		appsThread.start();
+		
+		/*Thread documentsThread = new Thread(documents,"documents");
+		documentsThread.start();*/
+		
+		
+		try {
+			systemThread.join();
+			appsThread.join();
+			/*documentsThread.join();*/
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Terminated: " + phaser.isTerminated());
+
+	}
+
+}
